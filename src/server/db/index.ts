f9381a5499f5f9769c26e5659c5ac84cc955d47c
KIPRI <hi@kipri.dev>
2023-08-08 18:46:52 +0300

    planetscale connection done using database url instead of host, user, pass

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,10 +2,6 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 import { env } from "~/env.mjs";
 
-const connection = connect({
-    host: env.DATABASE_HOST,
-    username: env.DATABASE_USERNAME,
-    password: env.DATABASE_PASSWORD,
-});
+const connection = connect({ url: env.DATABASE_URL });
 
 export const db = drizzle(connection);
